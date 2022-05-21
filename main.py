767eb7fc3abb8c276f66474d0b8ad15183206e80
ilmir <ilmir531@gmail.com>
2022-05-21 18:14:50 +0300

    Исправил замечания куратора 2

--- main.py
+++ main.py
@@ -15,106 +15,96 @@ def predict_salary(salary_from, salary_to):
         return int(salary_to * 1.2)
 
 
-def predict_rub_salary_hh(programing_languages):
-    salary = []
+def predict_rub_salary_hh(programming_language):
+    url = "https://api.hh.ru/vacancies/"
+    salaries = []
     for page in count(0):
-        param = {"text": f"Программист {programing_languages}",
-                 "area": "1",
-                 "page": f"{page}",
-                 "only_with_salary": "true"}
-        response = requests.get("https://api.hh.ru/vacancies/", params=param).json()
+        param = {
+            "text": f"Программист {programming_language}",
+            "area": "1",
+            "page": f"{page}",
+            "only_with_salary": "true"
+        }
+        response = requests.get(url, params=param)
+        response.raise_for_status()
+        decoded_response = response.json()
 
-        if not response["found"]:
-            return {"vacancies_found": 0, "vacancies_processed": 0,
-                    "average_salary": 0}
-
-        if page == response["pages"]:
+        if page == decoded_response["pages"]:
             break
 
-        for vacancy in response["items"]:
+        for vacancy in decoded_response["items"]:
             if vacancy["salary"]["currency"] == "RUR":
                 salary_from = vacancy["salary"]["from"]
                 salary_to = vacancy["salary"]["to"]
-                salary.append(predict_salary(salary_from, salary_to))
+                salary = predict_salary(salary_from, salary_to)
+                if not salary:
+                    continue
+                salaries.append(salary)
 
-    data_filtering = [i for i in salary if i is not None]
-    average_salary = int(sum(data_filtering) / len(data_filtering))
-    return {"vacancies_found": response["found"], "vacancies_processed": len(data_filtering),
-            "average_salary": average_salary}
+    average_salary = int(sum(salaries) / len(salaries)) if salaries else 0
+    return {
+        "vacancies_found": decoded_response["found"],
+        "vacancies_processed": len(salaries),
+        "average_salary": average_salary
+    }
 
 
-def predict_rub_salary_sj(programing_languages, x_api_app_id, authorization):
+def predict_rub_salary_sj(programming_language, x_api_app_id, authorization):
+    url = "https://api.superjob.ru/2.0/vacancies/"
     header = {
         "X-Api-App-Id": x_api_app_id,
         "Authorization": authorization,
     }
-    salary = []
+    salaries = []
     for page in count(0):
-        parametrs = {"page": f"{page}",
-                     "catalogues": "48",
-                     "t": "4",
-                     "keyword": f"Программист {programing_languages}",
-                     "more": "true", }
-        response = requests.get("https://api.superjob.ru/2.0/vacancies/", headers=header, params=parametrs).json()
-        if not response["total"]:
-            return {"vacancies_found": 0, "vacancies_processed": 0,
-                    "average_salary": 0}
-        for vacancy in response["objects"]:
+        param = {
+            "page": f"{page}",
+            "catalogues": "48",
+            "t": "4",
+            "keyword": f"Программист {programming_language}",
+            "more": "true"
+        }
+        response = requests.get(url, headers=header, params=param)
+        response.raise_for_status()
+        decoded_response = response.json()
+
+        for vacancy in decoded_response["objects"]:
             if vacancy["currency"] == "rub":
                 salary_from = vacancy["payment_from"]
                 salary_to = vacancy["payment_to"]
-                salary.append(predict_salary(salary_from, salary_to))
-        if not response["more"]:
+                salary = predict_salary(salary_from, salary_to)
+                if not salary:
+                    continue
+                salaries.append(salary)
+        if not decoded_response["more"]:
             break
-    data_filtering = [i for i in salary if i is not None]
-    average_salary = int(sum(data_filtering) / len(data_filtering))
-    return {"vacancies_found": response["total"], "vacancies_processed": len(data_filtering),
-            "average_salary": average_salary}
 
-
-def show_table(superjob_stats, headhunter_stats):
-    superjob_table = [
-        ["Язык программирования", "Вакансий найдено", "Вакансий обработано", "Средняя зарплата"],
-        [superjob_stats[0][0], superjob_stats[0][1], superjob_stats[0][2], superjob_stats[0][3]],
-        [superjob_stats[1][0], superjob_stats[1][1], superjob_stats[1][2], superjob_stats[1][3]],
-        [superjob_stats[2][0], superjob_stats[2][1], superjob_stats[2][2], superjob_stats[2][3]],
-        [superjob_stats[3][0], superjob_stats[3][1], superjob_stats[3][2], superjob_stats[3][3]],
-        [superjob_stats[4][0], superjob_stats[4][1], superjob_stats[4][2], superjob_stats[4][3]],
-        [superjob_stats[5][0], superjob_stats[5][1], superjob_stats[5][2], superjob_stats[5][3]],
-        [superjob_stats[6][0], superjob_stats[6][1], superjob_stats[6][2], superjob_stats[6][3]],
-        [superjob_stats[7][0], superjob_stats[7][1], superjob_stats[7][2], superjob_stats[7][3]],
-        [superjob_stats[8][0], superjob_stats[8][1], superjob_stats[8][2], superjob_stats[8][3]],
-        [superjob_stats[9][0], superjob_stats[9][1], superjob_stats[9][2], superjob_stats[9][3]],
-
-    ]
-    headhunter_table = [
-        ["Язык программирования", "Вакансий найдено", "Вакансий обработано", "Средняя зарплата"],
-        [headhunter_stats[0][0], headhunter_stats[0][1], headhunter_stats[0][2], headhunter_stats[0][3]],
-        [headhunter_stats[1][0], headhunter_stats[1][1], headhunter_stats[1][2], headhunter_stats[1][3]],
-        [headhunter_stats[2][0], headhunter_stats[2][1], headhunter_stats[2][2], headhunter_stats[2][3]],
-        [headhunter_stats[3][0], headhunter_stats[3][1], headhunter_stats[3][2], headhunter_stats[3][3]],
-        [headhunter_stats[4][0], headhunter_stats[4][1], headhunter_stats[4][2], headhunter_stats[4][3]],
-        [headhunter_stats[5][0], headhunter_stats[5][1], headhunter_stats[5][2], headhunter_stats[5][3]],
-        [headhunter_stats[6][0], headhunter_stats[6][1], headhunter_stats[6][2], headhunter_stats[6][3]],
-        [headhunter_stats[7][0], headhunter_stats[7][1], headhunter_stats[7][2], headhunter_stats[7][3]],
-        [headhunter_stats[8][0], headhunter_stats[8][1], headhunter_stats[8][2], headhunter_stats[8][3]],
-        [headhunter_stats[9][0], headhunter_stats[9][1], headhunter_stats[9][2], headhunter_stats[9][3]],
-
-    ]
-
-    superjob_moscow_title = 'SuperJob Moscow'
-    headhunter_moscow_title = 'HeadHunter Moscow'
-    table_instance_superjob = AsciiTable(superjob_table, superjob_moscow_title)
-    table_instance_headhunter = AsciiTable(headhunter_table, headhunter_moscow_title)
-    print(table_instance_headhunter.table)
-    print()
-    print(table_instance_superjob.table)
+    average_salary = int(sum(salaries) / len(salaries)) if salaries else 0
+    return {
+        "vacancies_found": decoded_response["total"],
+        "vacancies_processed": len(salaries),
+        "average_salary": average_salary
+    }
 
 
 def get_table(table_stats):
-    table = []
+    table = [
+        [
+            "Язык программирования",
+            "Вакансий найдено",
+            "Вакансий обработано",
+            "Средняя зарплата"
+        ]
+    ]
     for lang, stats, in table_stats.items():
-        table.append([lang, stats["vacancies_found"], stats["vacancies_processed"], stats["average_salary"]])
+        table.append(
+            [
+                lang,
+                stats["vacancies_found"],
+                stats["vacancies_processed"],
+                stats["average_salary"]
+            ]
+        )
     return table
 
 
@@ -123,22 +113,39 @@ def main():
     x_api_app_id = os.getenv("X_API_APP_ID")
     authorization = os.getenv("AUTHORIZATION")
 
-    programing_languages = ["GO", "JavaScript",
-                            "Java", "Python",
-                            "Ruby", "PHP",
-                            "C++", "C#",
-                            "C", "TypeScript",
-                            ]
+    programing_languages = [
+        "GO",
+        "JavaScript",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+        "C++",
+        "C#",
+        "C",
+        "TypeScript",
+
+    ]
 
     sj_language_statistics = {}
     hh_language_statistics = {}
     for language in programing_languages:
         hh_language_statistics[language] = predict_rub_salary_hh(language)
-        sj_language_statistics[language] = predict_rub_salary_sj(language, x_api_app_id, authorization)
-
-    sj_table_stats = get_table(sj_language_statistics)
-    hh_table_stats = get_table(hh_language_statistics)
-    show_table(sj_table_stats, hh_table_stats)
+        sj_language_statistics[language] = predict_rub_salary_sj(
+            language,
+            x_api_app_id,
+            authorization
+        )
+    print(sj_language_statistics)
+    sj_table = get_table(sj_language_statistics)
+    hh_table = get_table(hh_language_statistics)
+    sj_moscow_title = 'SuperJob Moscow'
+    headhunter_moscow_title = 'HeadHunter Moscow'
+    table_instance_sj = AsciiTable(sj_table, sj_moscow_title)
+    table_instance_headhunter = AsciiTable(hh_table, headhunter_moscow_title)
+    print(table_instance_headhunter.table)
+    print()
+    print(table_instance_sj.table)
 
 
 if __name__ == "__main__":
